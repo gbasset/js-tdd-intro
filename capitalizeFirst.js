7bfd3511b99940ecc98c13c590c91ef51dd013e1
Gaetan <gaetan@protonmail.com>
2020-01-24 10:57:54 +0100

    make a new function tdd

--- capitalizeFirst.js
+++ capitalizeFirst.js
@@ -1,22 +1,32 @@
 
 // capitalizeFirst.js
+const assert = require('assert');
+
 function capitalizeFirst(input) {
-  return input.length > 0
-    ? input[0].toUpperCase() + input.slice(1)
-    : '';
+  let output = ''
+  for ( i=0; i<input.length; i++){
+    if(i === 0){
+      output += input[0].toUpperCase()
+    }
+    else {output += input[i]}
+  }
+  return output
+
 }
 
-
 // function capitalizeFirst(input) {
-//     let output = '';
-//     for (let i = 0 ; i < input.length ; i++) {
-//       if (i === 0) {
-//         output += input[0].toUpperCase();
-//       } else {
-//         output += input[i];
-//       }
-//     }
-//     return output;
-//   }
+//   return input.length > 0
+//     ? input[0].toUpperCase() + input.slice(1)
+//     : '';
+// }
+
+    assert.strictEqual(typeof capitalizeFirst, 'function');
+    assert.strictEqual(capitalizeFirst.length, 1);
+    assert.strictEqual(capitalizeFirst('javaScript'), 'JavaScript');
+    assert.strictEqual(capitalizeFirst('z'), 'Z');
+    assert.strictEqual(capitalizeFirst(''), '');
+
+
+
 
 module.exports = capitalizeFirst;
